b52b1f538ceefe5cf5cf71839c65959fc86bfab2
sergio orozco <sergiooro311@gmail.com>
2023-06-19 22:50:07 -0500

    created guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Sergio's Gussing Game!!!");
+    println!("Please Input your Guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to Read??");
+
+    print!("You have guess: {guess}")
+
 }
