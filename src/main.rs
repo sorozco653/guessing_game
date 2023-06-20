724b4eee622c7ba61581d780f684a4f1c3499d90
sergio orozco <sergiooro311@gmail.com>
2023-06-19 23:36:02 -0500

    basic number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Welcome to Sergio's Gussing Game!!!");
-    println!("Please Input your Guess");
+    println!("Next Game!");
+    println!("Guess the Number Between 1 and 100!");
+
+    let range = 1..=100;
+    let secret_number = rand::thread_rng().gen_range(range);
+
+    println!("Input your guess!");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to Read??");
+        .expect("Failed to read line");
 
-    print!("You have guess: {guess}")
+    let number_guess = guess.trim().parse::<u32>().expect(" I guess lets try error handling?");
 
+    println!("You have guess {guess}");
+
+    match number_guess.cmp(&secret_number) {
+        Ordering::Greater => println!("Too Big Fool"),
+        Ordering::Less => println!("Too small Imbecile"),
+        Ordering::Equal => println!("Exactly That!!")
+    };
+
+    println!("The correct number is {secret_number}")
 }
