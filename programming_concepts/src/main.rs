851d04473164be0b20437092347dd1ab526c8a99
sergio orozco <sergiooro311@gmail.com>
2023-06-20 23:34:24 -0500

    lesson data types

--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // Let + Mut
     let mut x = 5;
@@ -20,4 +22,35 @@ fn main() {
     }
 
     println!("The value of x is: {x}"); // Variable are block scope so the last value of x was 11
+
+    // Tuples
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+    println!("The value of z is: {z}");
+
+    invalid_arr()
+}
+
+fn invalid_arr () {
+    let a = [1,2,3,4,5];
+
+    println!("Please Enter an array index");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index = index
+        .trim()
+        .parse::<usize>()
+        .expect("Index was not a number dude");
+
+    let element = a[index];
+
+    println!("Element: {element}, Index: {index}")
 }
